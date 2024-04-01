1318d0006fc019af7ed4afe72bb3d039c795ba0a
Priyanshu Garg <priyanshu.garg02@nagarro.com>
2024-04-01 15:51:59 +0530

    docker file added

--- src/main/java/com/nagarro/training/DemoApplication/DemoApplication.java
+++ src/main/java/com/nagarro/training/DemoApplication/DemoApplication.java
@@ -2,10 +2,17 @@ package com.nagarro.training.DemoApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to this application.";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
